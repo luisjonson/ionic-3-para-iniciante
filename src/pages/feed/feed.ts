ac0cefb8707d209784b1f2f0f208bf693f13d5d4
jonson <mocoto.xy@hotmail.com>
2018-10-14 20:44:16 -0300

    first commit

---
+++ src/pages/feed/feed.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the FeedPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-feed',
+  templateUrl: 'feed.html',
+})
+export class FeedPage {
+// variavel 
+  public nome_usuario:string = "Luis Lindonjonson";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  //função 
+  public somaDeDoisNumero(num1:number,num2:number): void{
+    alert(num1 + num2);
+  }
+
+  ionViewDidLoad() {
+    this.somaDeDoisNumero(10,20);
+  }
+
+}
